e5fee1a9df78530f50ec8c1422b066a9ffd89824
Ahmed Ghannam <iia7mt@gmail.com>
2023-02-08 11:55:25 +0200

    add to testcases and update excute function along with it

--- src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
+++ src/main/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractor.kt
@@ -6,8 +6,14 @@ class GetCityHasCheapestInternetConnectionInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
 
-    fun execute(): CityEntity {
-       throw NotImplementedError()
+    fun execute(): CityEntity? {
+        val list = dataSource.getAllCitiesData()
+        if (list.isEmpty()) return null
+        return dataSource
+            .getAllCitiesData()
+            .filter(::excludeNullSalaries)
+            .sortedBy { it.servicesPrices.internet60MbpsOrMoreUnlimitedDataCableAdsl?.div(it.averageMonthlyNetSalaryAfterTax!!) }
+            .first()
     }
 
     private fun excludeNullSalaries(city: CityEntity): Boolean {
--- src/test/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractorTest.kt
+++ src/test/kotlin/interactor/GetCityHasCheapestInternetConnectionInteractorTest.kt
@@ -1,12 +1,61 @@
 package interactor
 
+import dataSource.CsvDataSource
+import dataSource.utils.CsvParser
 import org.junit.jupiter.api.Test
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
 
 class GetCityHasCheapestInternetConnectionInteractorTest {
+    private lateinit var cheapestInternet:GetCityHasCheapestInternetConnectionInteractor
 
     @Test
-    fun execute() {
+    fun should_ReturnLeastInterNetCost_When_TheListNotEmptyAndNotNullAndTheAttributesNotNull() {
+        // given list of cities
+        val csvParser = CsvParser()
+        val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser, SUB_FILE_NAME) // CSV : comma separated values
+        cheapestInternet=GetCityHasCheapestInternetConnectionInteractor(dataSource)
+        val list=dataSource.getAllCitiesData()
+        // when find the minimum internet coast of this list
+        val cheapestCityInInternet=cheapestInternet.execute()
+        // then
+        assertEquals(list[1],cheapestCityInInternet)
+
+    }
+
+    @Test
+    fun should_ReturnNull_When_TheFileIsEmpty() {
+        // given empty csv file
+        val csvParser = CsvParser()
+        val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser, EMPTY_FILE) // CSV : comma separated values
+        cheapestInternet=GetCityHasCheapestInternetConnectionInteractor(dataSource)
+        val list=dataSource.getAllCitiesData()
+        // when find the minimum internet coast of this list
+        val cheapestCityInInternet=cheapestInternet.execute()
+        // then
+        assertNull(cheapestCityInInternet)
+
+    }
+    @Test
+    fun should_ReturnNull_When_TheSalaryIsNull() {
+//        // given a null salary cities list
+//        val csvParser = CsvParser()
+//        val dataSource: CostOfLivingDataSource = CsvDataSource(csvParser, EMPTY_FILE) // CSV : comma separated values
+//        cheapestInternet=GetCityHasCheapestInternetConnectionInteractor(dataSource)
+//        val list=dataSource.getAllCitiesData()
+//        // when find the minimum internet coast of this list
+//        val cheapestCityInInternet=cheapestInternet.execute()
+//        // then
+//        assertNull(cheapestCityInInternet)
+
+    }
+
+
+
+    companion object{
+        private const val SUB_FILE_NAME = "subListOfCostOfLiving.csv"
+        private const val EMPTY_FILE = "emptyFile.csv"
     }
 }
