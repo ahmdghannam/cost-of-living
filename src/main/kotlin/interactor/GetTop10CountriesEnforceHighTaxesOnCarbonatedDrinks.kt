039bcd5d6643486d2fed4f9a1cac0e8f736e3e47
Yaseen Zaqut <yz456359@gmail.com>
2023-02-12 16:18:46 +0200

    implement the required function

--- src/main/kotlin/interactor/GetTop10CountriesEnforceHighTaxesOnCarbonatedDrinks.kt
+++ src/main/kotlin/interactor/GetTop10CountriesEnforceHighTaxesOnCarbonatedDrinks.kt
@@ -1,9 +1,23 @@
 package interactor
 
+import model.CityEntity
+
 class GetTop10CountriesEnforceHighTaxesOnCarbonatedDrinks (
     private val dataSource: CostOfLivingDataSource,
 ){
     fun excute():List<String>?{
+        val list = dataSource.getAllCitiesData()
+        if (list.isEmpty()) return null
+
+        return dataSource.getAllCitiesData()
+            .filter(::excludeNullPricesAndLowQualityData)
+            .sortedByDescending{it.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants}
+            .take(10).map { it.cityName}
 
     }
+    private fun excludeNullPricesAndLowQualityData(city: CityEntity): Boolean {
+        return city.drinksPrices.cokePepsiAThirdOfLiterBottleInRestaurants != null
+                && city.dataQuality
+    }
+
 }
