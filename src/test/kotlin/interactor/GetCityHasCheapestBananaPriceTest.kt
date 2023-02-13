0917b28939ab3353b7bc36030af9a377763d649b
AbdAlKareem M. Alaraj <kareem.m.alaraj@gmail.com>
2023-02-13 11:40:04 +0200

    add test class

---
+++ src/test/kotlin/interactor/GetCityHasCheapestBananaPriceTest.kt
@@ -0,0 +1,10 @@
+package interactor
+
+import org.junit.jupiter.api.Assertions.*
+
+class GetCityHasCheapestBananaPriceTest{
+
+    private lateinit var cheapestBanana: GetCityHasCheapestBananaPriceInteractor
+
+
+}
