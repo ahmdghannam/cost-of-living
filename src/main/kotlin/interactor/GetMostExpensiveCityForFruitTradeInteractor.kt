c264ca13c6bc6b3c2fa6b06d9ead3031e29d1fd0
mohammed akkad <momoo362514@gmail.com>
2023-02-17 18:03:00 +0200

    add execute function

--- src/main/kotlin/interactor/GetMostExpensiveCityForFruitTradeInteractor.kt
+++ src/main/kotlin/interactor/GetMostExpensiveCityForFruitTradeInteractor.kt
@@ -1,9 +1,36 @@
 package interactor
 
+import model.CityEntity
+
 class GetMostExpensiveCityForFruitTradeInteractor(
     private val dataSource: CostOfLivingDataSource,
 ) {
-    fun execute(nameFruit : String?) : String{
-        return  ""
+    fun execute(nameFruit: String?): String {
+        val cities = dataSource
+            .getAllCitiesData()
+            .filter(::excludeNullFoodPrices)
+        return when (nameFruit!!.toLowerCase().trim()) {
+            "apple" -> "Apple from ${cities.minByOrNull { it.fruitAndVegetablesPrices.apples1kg!! }!!.cityName} " +
+                    "to ${cities.maxByOrNull { it.fruitAndVegetablesPrices.apples1kg!! }!!.cityName}"
+
+            "banana" -> "Banana from ${cities.minByOrNull { it.fruitAndVegetablesPrices.banana1kg!! }!!.cityName} " +
+                    "to ${cities.maxByOrNull { it.fruitAndVegetablesPrices.banana1kg!! }!!.cityName}"
+
+            "orange" -> "Orange from ${cities.minByOrNull { it.fruitAndVegetablesPrices.oranges1kg!! }!!.cityName} " +
+                    "to ${cities.maxByOrNull { it.fruitAndVegetablesPrices.oranges1kg!! }!!.cityName}"
+
+            else -> "Please write the name of the fruit correctly, such as an apple, banana, or orange"
+        }
+    }
+
+    private fun excludeNullFoodPrices(city: CityEntity): Boolean {
+        return city.fruitAndVegetablesPrices.let {
+            it.apples1kg != null &&
+                    it.banana1kg != null &&
+                    it.oranges1kg != null
+
+        }
+
     }
 }
+
